0725d14eecd7841bb6f9fd0dacc53e9a9ae0948e
Juan Carlos Velásquez <velasquez.gaviria@.gmail.com>
2021-05-15 19:15:09 -0500

    Fix short summary

--- btclab/crypto.py
+++ btclab/crypto.py
@@ -72,7 +72,7 @@ def place_order(exchange, symbol, price, amount_in_usd, dry_run=True):
     return order
 
 
-def short_summary(order, pct_chg, buy_again: bool) -> str:
+def short_summary(order, pct_chg) -> str:
     """ "
     Returns a brief description of an order
     order param is a dict with the structure defined in
